f4dedaa811f8b33cbddd89a6f7d6b86f295b03df
Jonathan Taylor <jonyonson@icloud.com>
2019-06-01 18:37:25 -0500

    left off at start of mongodb section

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 
 export const app = express()
+const router = express.Router()
 
 app.disable('x-powered-by')
 
@@ -12,4 +13,20 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+router.get('/me', (req, res) => {
+  res.send({ me: 'hello' })
+})
+
+app.get('/data', (req, res) => {
+  res.send({ data: [1, 2, 3] })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
